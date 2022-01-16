52954faa9a7183ab68c747368a4ee91d08403a0e
chiang-song-lang <afga3780@gmail.com>
2022-01-16 12:16:20 +0800

    add module

---
+++ module/app.js
@@ -0,0 +1,7 @@
+let car = require("./car");
+
+console.log(car.color);
+car.color = "blue";
+console.log(car.color);
+car.setName("Lily");
+car.showName();
---
+++ module/car.js
@@ -0,0 +1,16 @@
+// wxports = module.exports = {};
+
+exports.color = "red";
+
+let name = "default";
+
+exports.setName = function (newName) {
+    name = newName;
+};
+
+exports.showName = function () {
+    console.log(`hello, ${name}`);
+};
+
+
+//return module.exports = {serName, showName}
